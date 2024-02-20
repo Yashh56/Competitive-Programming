eefa876c078f471ea8c68770bbcb3bf64b1f2b53
Yash <yashsaini18166@gmail.com>
2024-02-20 22:37:33 +0530

    BS Practice

---
+++ BinarySearch/SearchInRotatedArray.java
@@ -0,0 +1,32 @@
+package BinarySearch;
+
+/**
+ * SearchInRotatedArray
+ */
+public class SearchInRotatedArray {
+    public int search(int[] arr, int target) {
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target <= arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (arr[mid] <= target && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
