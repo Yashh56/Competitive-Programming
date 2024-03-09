9524b468e90359cc566e7f757ebe7d03f673aecc
Yash <yashsaini18166@gmail.com>
2024-03-09 22:38:11 +0530

    BS Medium

---
+++ BinarySearch/MedianTwoSorted.java
@@ -0,0 +1,34 @@
+package BinarySearch;
+
+class Solution {
+    public double findMedianSortedArrays(int[] a, int[] b) {
+        int n1 = a.length;
+        int n2 = b.length;
+        if (n1 > n2)
+            return findMedianSortedArrays(b, a);
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2;
+        int low = 0;
+        int high = n1;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+
+            int l1 = (mid1 > 0) ? a[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? b[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? a[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? b[mid2] : Integer.MAX_VALUE;
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1)
+                    return Math.max(l1, l2);
+                else
+                    return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            } else if (l1 > l2)
+                high = mid1 - 1;
+            else
+                low = mid1 + 1;
+        }
+        return 0;
+    }
+}
---
+++ BinarySearch/PeakElement.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class PeakElement {
+    public int findPeakElement(int[] arr) {
+        int n = arr.length;
+        if (n == 1)
+            return 0;
+        if (arr[0] > arr[1])
+            return 0;
+        if (arr[n - 1] > arr[n - 2])
+            return n - 1;
+        int low = 1;
+        int high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+
+            if (arr[mid] > arr[mid - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
