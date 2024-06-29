07645cde0aa8320a7575fd42a7f14dc11aad05b5
Yash <yashsaini18166@gmail.com>
2024-06-29 19:26:32 +0530

    Practice

---
+++ Arrays/MediumProblems/K-diff Pairs in an Array.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    int findPairs(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+        set<pair<int, int>> ans;
+
+        int n = nums.size();
+        int i = 0;
+        int j = 1;
+        while (j < n)
+        {
+            int diff = nums[j] - nums[i];
+            if (diff == k)
+            {
+                ans.insert({nums[i], nums[j]});
+                i++;
+                j++;
+            }
+            else if (diff > k)
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+            if (i == j)
+            {
+                j++;
+            }
+        }
+        return ans.size();
+    }
+};
