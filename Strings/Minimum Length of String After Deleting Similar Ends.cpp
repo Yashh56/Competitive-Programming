bbbf4a8285190b5b4f42a1c99802871029076ea4
Yash <yashsaini18166@gmail.com>
2024-07-04 23:21:27 +0530

    Create Minimum Length of String After Deleting Similar Ends

---
+++ Strings/Minimum Length of String After Deleting Similar Ends.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int n = s.size();
+        int i = 0 , j = n - 1;
+        while(i<j && s[i]==s[j]){
+            char ch = s[i];
+            while(i<j && s[i]==ch){
+                i++;
+            }
+            while(j>= i && s[j]==ch){
+                j--;
+            }
+        }
+        return j-i+1;
+    }
+};
