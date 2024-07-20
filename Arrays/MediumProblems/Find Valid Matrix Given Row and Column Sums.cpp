a3c2f23fd25618c9d1cbd63f7892a4c4d51bd23f
Yash <yashsaini18166@gmail.com>
2024-07-20 23:29:45 +0530

    Create Find Valid Matrix Given Row and Column Sums.cpp

---
+++ Arrays/MediumProblems/Find Valid Matrix Given Row and Column Sums.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int n = rowSum.size();
+        int m = colSum.size();
+        vector<vector<int>> matrix(n,vector<int>(m));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(rowSum[i]<colSum[j]){
+                    matrix[i][j] = rowSum[i];
+                }else if(rowSum[i]>colSum[j]){
+                    matrix[i][j] = colSum[j];
+                }else{
+                    matrix[i][j] = colSum[j];
+                }
+                rowSum[i] -= matrix[i][j];
+                colSum[j] -= matrix[i][j];
+            }
+        }
+        return matrix;
+    }
+};
