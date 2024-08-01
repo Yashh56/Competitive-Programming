4708a6c8f2bdb6a6f1971511fd9861a4874bae4f
Yash <yashsaini18166@gmail.com>
2024-08-01 23:21:23 +0530

    Create Number of Senior Citizen.cpp

---
+++ LeetCode-Problems/Arrays/EasyProblems/Number of Senior Citizen.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int cnt = 0;
+        for(string &s:details){
+            cnt+=stoi(s.substr(11,2))>60;
+        }
+        return cnt;
+
+    }
+};
