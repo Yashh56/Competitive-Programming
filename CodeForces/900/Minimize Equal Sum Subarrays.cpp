e688de51f38be18f0bd4143e6661c73d8889dd81
Yash <yashsaini18166@gmail.com>
2024-08-15 22:50:47 +0530

    Create Minimize Equal Sum Subarrays.cpp

---
+++ CodeForces/900/Minimize Equal Sum Subarrays.cpp
@@ -0,0 +1,120 @@
+// Author : Yashh56
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define sp ' '
+#define pi 2 * acos(0.0)
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define Yash                    \
+    cin.sync_with_stdio(false); \
+    cin.tie(0);                 \
+    cout.tie(0)
+
+// Control Flow
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b) - 1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a) - 1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+// #define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+// Types
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define int long long
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+
+// Utils
+#define len(x) int((x).size())
+#define pb push_back
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+bool odd(ll num) { return ((num & 1) == 1); }
+bool even(ll num) { return ((num & 1) == 0); }
+
+// Utils
+#define len(x) int((x).size())
+#define pb push_back
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+// const int MOD = 1e9 + 7;
+int GCD(int a, int b)
+{
+    return b == 0 ? a : GCD(b, a % b);
+}
+
+const int mod = 1e9 + 7;
+
+int power(int x, int y, int MOD = mod)
+{
+    if (y == 0)
+    {
+        return 1;
+    }
+    if (y % 2 == 0)
+    {
+        return power((x * x) % MOD, y / 2, MOD) % MOD;
+    }
+    else
+    {
+        return (x * power((x * x) % MOD, (y - 1) / 2, MOD) % MOD) % MOD;
+    }
+}
+
+int inverse(int n, int p)
+{
+    return power(n, p - 2, p);
+}
+
+const int MAX = 200'007;
+
+/////////////////////////////////////////////////// SOLUTION HERE ////////////////////////////////////////////////////
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vi a(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+
+    vi ans(n);
+    for (int i = 0; i < n - 1; ++i)
+    {
+        ans[i] = a[i + 1];
+    }
+    ans[n - 1] = a[0];
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << nl;
+}
+
+int32_t main()
+{
+    Yash;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
