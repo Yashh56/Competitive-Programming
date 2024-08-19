816ba1dd7b7a7f4da4edb2df7be92ef25f3ac1fa
Yash <yashsaini18166@gmail.com>
2024-08-19 17:08:43 +0530

    1000

---
+++ CodeForces/1000/Distanced Coloring.cpp
@@ -0,0 +1,221 @@
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠟⢿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠹⣿⣾⣿⣿⡿⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⡟⠀⠀⣠⣄⡀⠀⢙⠿⠿⠟⠁⠀⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠟⢙⣧⠀⠀⠻⣿⣿⣷⣦⣆⠄⣀⣠⣶⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣤⣾⣿⠀⠀⠀⡆⣿⣿⡿⠛⠙⣿⣿⠋⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⢸⣿⣿⣇⠀⠀⠉⠛⢣⠄⠀⠀⠈⡟⠈⠁⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡽⢾⣿⣿⣿⣶⣾⣿⣿⣿⣿⣷⣶⣶⣤⣤⣴⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠊⠉⠀⣼⣿⣿⣿⣿⠿⠋⠉⠙⠛⠛⠻⠿⠿⣿⣿⡷⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⠉⠀⠀⠀⠀⢸⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⠀⠉⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡞⠁⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⣿⣿⣦⣤⣀⣀⠀⠀⣀⣼⣿⣿⠁⠀⠀⠀⠀⠈⠳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠹⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⠿⠿⣿⣿⣿⣿⡿⠿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠀⠀⠀⠀⠀⢀⣳⣄⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠁⠀⠀⠀⠀⠀⠀⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⠀⠀⠀⠀⠀⠀⢠⡏⠀⠀⣠⠖⠋⠁⣀⡀⠙⠳⣄⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀⠀⠀⠀⠀⠀⠀⠈⢳⣄⣀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠟⠀⠀⠀⠀⠀⢀⣠⡾⣀⡀⣰⢁⡤⠞⠋⠁⠈⠙⢦⠈⢳⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡏⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣁⣉⣉⣓⠶⠶⠶⠶⠿⠿⠷⠖⠚⠛⠒⠒⠒⠛⠓⢷⣀⡽⠿⠋⢉⡉⠉⠲⡆⠀⠀⢧⠀⣧⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠧⠤⣤⣄⡀⠀⠀⠀⣠⠏⢾⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠞⠉⣀⣴⣾⠋⠙⠛⠒⠃⠀⠀⢸⠀⡿⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠏⠀⠀⠀⠀⠀⠉⠳⢄⣠⣧⣄⣸⣀⣀⣀⣀⣀⣀⣀⣀⣤⣤⣤⣤⣤⡤⠤⣤⠖⠋⢀⡴⠺⣅⣤⢯⠳⣤⣀⢀⡀⠀⠀⣿⣸⠇⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣧⣀⣀⡀⠀⠀⠀⠀⠀⢿⡀⢀⡟⠉⠉⣉⣩⣿⠉⠉⠁⠀⠀⠀⠀⠀⣠⠞⠁⣠⠖⠉⠀⠀⢿⢷⠈⠳⣄⣉⠉⠀⢠⣼⠟⠁⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡏⠉⠉⠉⠑⠒⢶⠴⠏⠹⣏⣠⡶⠋⠁⠀⠙⢦⠀⠀⠀⠀⠀⢠⡞⠁⣠⠞⠁⠀⠀⠀⣀⣾⡈⡷⢤⣈⣩⠴⠞⡉⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠲⣄⠘⡄⠀⣼⣿⠟⠁⠀⠀⠀⠀⠈⢳⣤⠤⠤⢴⠏⠀⣴⣥⣤⣤⠤⠴⠺⠯⣄⣿⠃⠀⣿⢁⡤⠚⣿⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⣀⡀⠀⠈⢣⣧⢞⡾⠃⠀⠀⠀⠀⠀⢀⣠⠞⠁⠀⢠⡏⠀⣼⠁⠀⠀⠀⠀⠀⢀⣤⣀⠈⢳⡤⠟⠃⠀⢠⡇⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀⠸⡇⡹⠋⠙⠲⢦⣤⣿⠟⠀⠀⠀⠀⣀⣠⠖⠋⠀⠀⢰⣖⡛⠲⠶⠧⣀⣀⣠⠀⠀⠉⠉⠉⠉⠑⠚⠦⡠⡄⢀⡜⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⣠⣴⠏⠱⡌⠉⠉⠉⠑⠒⢲⣿⠁⠀⠀⢠⣾⣿⠉⠀⣀⣀⡐⡟⠉⠀⠀⠀⠀⠀⠘⣇⠹⣆⡀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣎⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⣠⣾⠟⠁⠀⠀⠙⢆⠀⠀⠀⣴⡿⢻⡇⠀⡴⠋⣿⡟⠉⠉⠉⠀⠉⢻⡄⠀⠀⠀⠀⠀⠀⢸⡷⣄⡉⠳⣄⡅⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⢀⡴⠋⠀⠀⠀⠀⠀⠀⠈⢳⣠⢿⢥⡿⣾⣿⡞⠁⠀⣿⢿⠉⣽⠙⡟⣢⢤⣷⠀⠀⠀⠀⠀⠀⢸⡇⠀⡝⠳⣼⣞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢧⠀⠀⠀⠀⠀⠀⠀⠀
+// ⢺⠉⣠⣭⡉⠁⠐⢩⡤⡴⠚⠁⢿⣠⡷⠋⢹⠀⠀⠀⠘⡾⠵⣿⣤⠧⠼⣿⠋⠀⠀⠀⠀⠀⡀⢸⡇⠀⡇⠀⠁⠈⣩⣓⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢼⡄⠀⠀⠀⠀⠀⠀⠀
+// ⢸⣇⢿⣤⡟⠀⠀⢸⣧⠟⠀⢀⣤⠋⠀⠀⢸⡆⠀⠀⠀⣷⠀⠈⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠹⡾⡇⢰⡇⠀⠀⠢⡙⣾⣛⣻⠿⠆⠀⠀⠀⠀⠀⠀⠀⢸⡼⣦⠀⠀⠀⠀⠀⠀
+// ⠀⠙⡏⠉⠉⠉⠉⠉⠉⠉⠉⠉⣧⠀⠀⠀⠀⢣⡀⠀⠀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡇⢸⡇⠀⢧⠀⠉⠓⠀⠀⠀⠀⠳⣄⠀⠀⢀⣀⣀⣀⢱⣌⣦⠀⠀⠀⠀⠀
+// ⠀⠀⢻⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡀⠀⠀⢀⠞⣇⠀⠀⣇⠀⠀⠀⢸⡄⠀⠀⠀⣄⠀⠀⠀⠀⠘⡇⠈⣷⠀⠈⠳⠤⠤⢴⣾⠁⠀⠀⠈⣰⢯⡿⠛⠋⠏⠉⢉⢯⣆⠀⠀⠀⡔
+// ⠀⠀⠈⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠀⡴⢿⡏⣹⡄⠀⣿⠀⠀⠀⠀⣹⣄⠀⠀⠙⣆⠀⠀⠀⠀⣇⠀⢸⢧⡀⠀⠀⢀⡼⠹⣆⣀⠀⡾⠁⢠⣴⣶⡆⠀⣾⣿⣟⣿⡀⠀⣸⠃
+// ⠀⠀⠀⣽⡀⣀⣀⠀⣀⣀⣀⣀⣀⣹⠞⣴⢺⣽⡟⠀⠀⣿⠀⠀⠀⢸⠁⠈⠙⠒⠶⣿⠀⠀⠀⠀⢸⠀⠘⡄⠙⠦⣄⢸⠃⠀⠹⣏⢧⣷⠀⠸⣿⣿⠇⠘⠿⢟⡾⠘⢿⣶⠃⠀
+// ⠀⠀⠀⢷⢀⡖⠓⡄⠀⠀⣴⠲⡄⠀⠀⣹⣿⡟⠀⠀⠀⣿⢦⣄⣀⢸⡄⠀⠀⠀⠀⠸⡄⠀⠀⢀⣾⡆⠀⣷⠀⠀⠈⠻⣇⠀⠀⢸⡇⠙⢢⣄⡉⣉⣀⣤⢴⡿⠤⠤⠚⠃⠀⠀
+// ⠀⠀⠀⠈⢷⣿⣿⣥⣤⡤⣽⣾⠥⢤⣴⡿⠋⠀⠀⠀⢠⠇⠀⢀⠃⠙⣧⠀⠀⠀⠀⠀⣇⠀⠀⠀⣏⢧⠀⠈⢦⣠⠴⠋⠘⢿⣶⣋⣀⡤⣞⡭⡽⠿⠭⠟⠉⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⢾⣀⣀⡼⠤⠴⠟⠀⠀⠀⠀⠀⣿⠦⣄⣀⣿⠈⢧⡀⠀⠀⠀⢀⡴⠋⠀⢹⡈⠉⠉⢰⣽⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⠀⠀⡇⠸⡇⠀⠙⠲⠔⠚⠁⠀⠀⠀⠀⠙⠓⠚⠛⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀
+
+// Author : Yashh56
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <array>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <fstream>
+#include <istream>
+#include <climits>
+#include <map>
+#include <list>
+#include <cmath>
+#include <deque>
+#include <numeric>
+#include <unordered_set>
+#include <bitset>
+#include <chrono>
+#include <cstdint>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <random>
+#define endl '\n'
+#define shibo                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+#else
+#define debug(...) 911
+#endif
+using namespace std::chrono;
+using namespace std;
+auto start = high_resolution_clock::now();
+// lc w
+// cf epic round
+using namespace std;
+
+#define nl '\n'
+#define sp ' '
+#define pi 2 * acos(0.0)
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define Yash                    \
+    cin.sync_with_stdio(false); \
+    cin.tie(0);                 \
+    cout.tie(0)
+
+// Control Flow
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b) - 1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a) - 1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+// #define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+// Types
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define int long long
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+
+// Utils
+#define len(x) int((x).size())
+#define pb push_back
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+bool odd(ll num) { return ((num & 1) == 1); }
+bool even(ll num) { return ((num & 1) == 0); }
+
+// Utils
+#define len(x) int((x).size())
+#define pb push_back
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+// const int MOD = 1e9 + 7;
+
+ll mod = 1e9 + 7;
+ll MAXN = 1e1 + 1;
+ll facN = 1e1 + 1;
+vector<ll> sieveerato(MAXN, 1);
+vector<ll> spf(MAXN, 1);
+vector<ll> primes;
+vector<ll> f(facN), inv(facN), finv(facN);
+const ll inf = LLONG_MAX - 1;
+int GCD(int a, int b)
+{
+    return b == 0 ? a : GCD(b, a % b);
+}
+
+// const int mod = 1e9 + 7;
+
+int power(int x, int y, int MOD = mod)
+{
+    if (y == 0)
+    {
+        return 1;
+    }
+    if (y % 2 == 0)
+    {
+        return power((x * x) % MOD, y / 2, MOD) % MOD;
+    }
+    else
+    {
+        return (x * power((x * x) % MOD, (y - 1) / 2, MOD) % MOD) % MOD;
+    }
+}
+
+int inverse(int n, int p)
+{
+    return power(n, p - 2, p);
+}
+
+// 1.  Think DP.
+// 2.  Think greedy.
+// 3.  Think binary search.
+// 4.  Think sliding window/two pointer.
+// 5.  Think bit masking.
+// 6.  Think bit manipulation.
+// 7.  Think prefix.
+// 8.  Think difference array.
+// 9.  Think mapping.
+// 10. Think multiset.
+// 11. Think gcd/lcm/modulo arithmetic.
+// 12. Think in variables/expressions.
+// 13. Think bipartite.
+// 14. Think brute force.
+// Read and try to understand every single test case
+// Divide test cases into cases. Almost every test case is there for a reason.
+// Taking more time than expected? Open paint and start writing
+// Still can't find a pattern? Write a bruteforce and look for a pattern there.
+// Try to minimize code for A and B. It will almost always have a simple solution.
+// __builtin_popcount(x): the number of ones in the bit representation
+
+// find first point where prefix sum becomes negative after current index
+
+/////////////////////////////////////////////////// SOLUTION HERE ////////////////////////////////////////////////////
+
+void solve()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    if (n * m < k)
+    {
+        cout << n * m << nl;
+        return;
+    }
+    else
+    {
+        int tmp = min(n, k);
+        cout << tmp * min(m, k) << nl;
+        return;
+    }
+}
+
+int32_t main()
+{
+    Yash;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        solve();
+    }
+}
